6a25c0e846cf6b538ac3b5b6e744349d141c55bd
GITUSER <hudson@inboundnow.com>
2021-01-16 22:39:12 -0600

    no message

---
+++ build/static/forgepress.c6717b99a572349201bb.hot-update.js
@@ -0,0 +1,83 @@
+webpackHotUpdate("forgepress",{
+
+/***/ "./node_modules/@frontity/components/iframe.tsx":
+false,
+
+/***/ "./node_modules/@frontity/components/image.tsx":
+false,
+
+/***/ "./node_modules/@frontity/components/link/index.tsx":
+false,
+
+/***/ "./node_modules/@frontity/components/link/utils.ts":
+false,
+
+/***/ "./node_modules/@frontity/components/switch.tsx":
+false,
+
+/***/ "./node_modules/@frontity/hooks/use-in-view.ts":
+false,
+
+/***/ "./node_modules/@frontity/html2react/processors/iframe.tsx":
+false,
+
+/***/ "./node_modules/@frontity/html2react/processors/image.tsx":
+false,
+
+/***/ "./node_modules/@frontity/html2react/processors/link.tsx":
+false,
+
+/***/ "./node_modules/react-intersection-observer/react-intersection-observer.m.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/featured-media.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/header.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/index.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/link.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/list/index.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/loading.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/menu-icon.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/menu-modal.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/menu.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/nav.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/page-error.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/post.js":
+false,
+
+/***/ "./packages/mars-theme/src/components/title.js":
+false,
+
+/***/ "./packages/mars-theme/src/index.js":
+/*!******************************************!*\
+  !*** ./packages/mars-theme/src/index.js ***!
+  \******************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: ENOENT: no such file or directory, open 'C:\\\\Users\\\\Hudson Atwell\\\\Local Sites\\\\ForgePress\\\\ForgePress\\\\packages\\\\mars-theme\\\\src\\\\index.js'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3BhY2thZ2VzL21hcnMtdGhlbWUvc3JjL2luZGV4LmpzLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./packages/mars-theme/src/index.js\n");
+
+/***/ })
+
+})
