11b3cac945a6d782f8b410247c944175dbbbd4cf
GITUSER <hudson@inboundnow.com>
2021-01-26 20:17:20 -0600

    Changing staging to live

--- frontity.settings.js
+++ frontity.settings.js
@@ -99,7 +99,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://staging.forgepress.dev",
+          "url": "https://wordpress.forgepress.dev",
         },
       }
     },
